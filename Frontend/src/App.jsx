147523d7d497138ad396998e0df0d1a675ef0008
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-03-18 17:40:46 +0530

    navbar added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+import Home from './components/Home'
+
+const appRouter= createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/signup",
+    element: <Signup/>,
+  }
+])
 
 const App = () => {
   return (
-    <div className='text-4xl'>Hello</div>
+    <>
+        <RouterProvider router={appRouter}/>
+    </>
   )
 }
 
---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
+import { Button } from "./ui/button";
+import { Avatar, AvatarImage } from "./ui/avatar";
+import { User2, LogOutIcon } from "lucide-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const user = false;
+  return (
+    <>
+      <div className="bg-white">
+        <div className="flex items-center justify-between mx-auto max-w-7xl h-16">
+          <div>
+            <h1 className="text-2xl font-bold">
+              Job<span className="text-blue-500">Hunt</span>
+            </h1>
+          </div>
+          <div className="flex items-center gap-12">
+            <ul className="flex font-medium items-center gap-5">
+              <li>Home</li>
+              <li>Jobs</li>
+              <li>Browse</li>
+            </ul>
+            {!user ? (
+              <div className="flex items-center gap-2">
+
+                <Link to="/login"><Button variant="outline">Login</Button></Link>
+                <Link to="/signup"><Button className="bg-blue-500 text-white hover">Signup</Button></Link>
+              </div>
+            ) : (
+              <Popover className="cursor-pointer">
+                <PopoverTrigger asChild>
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                  </Avatar>
+                </PopoverTrigger>
+                <PopoverContent className="w-80">
+                  <div className="flex gap-4 space-y-2">
+                    <Avatar>
+                      <AvatarImage
+                        src="https://github.com/shadcn.png"
+                        alt="@shadcn"
+                      />
+                    </Avatar>
+                    <div>
+                      <h4 className="font-medium">Manpreet Singh</h4>
+                      <p className="text-sm text-muted-foreground">
+                        lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex flex-col my-2 text-gray-600">
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <User2 className="w-4 h-4" />
+                      <Button variant="link"> View Profile</Button>
+                    </div>
+                    <div className="flex w-fit items-center gap-2 cursor-pointer">
+                      <LogOutIcon className="w-4 h-4" />
+                      <Button variant="link"> Logout</Button>
+                    </div>
+                  </div>
+                </PopoverContent>
+              </Popover>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Navbar;
