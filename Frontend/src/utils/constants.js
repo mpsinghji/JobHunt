23749072ab5496d0fe39faef6853b1ff732d58fe
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-05-08 18:31:33 +0530

    minor fix

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,5 +1,4 @@
-const phase = import.meta.env.NODE_ENV === "development" ? "dev" : "prod";
-const BASE_BACKEND_URL = phase === "dev" ? `${import.meta.env.LOCALHOST_URL}` : `${import.meta.env.BACKEND_WEB_URL}`;
+const BASE_BACKEND_URL = "http://localhost:3000"
 
 export const USER_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/user`;
 export const COMPANY_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/company`;
