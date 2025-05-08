4124fd90f0869fb8750116e03bfbd9b16092536c
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-05-08 12:22:51 +0530

    trying production #2

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-const phase = process.env.NODE_ENV === "development" ? "dev" : "prod";
-const BASE_BACKEND_URL = phase === "dev" ? "http://localhost:3000" : "https://jobhunt-73ih.onrender.com"
+const phase = import.meta.env.NODE_ENV === "development" ? "dev" : "prod";
+const BASE_BACKEND_URL = phase === "dev" ? `${import.meta.env.LOCALHOST_URL}` : `${import.meta.env.BACKEND_WEB_URL}`;
 
 export const USER_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/user`;
 export const COMPANY_API_END_POINT = `${BASE_BACKEND_URL}/api/v1/company`;
